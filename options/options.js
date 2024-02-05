4ffd002e70f14bcd2c876803a66ee74b18694ddb
Crowdbotics <team@crowdbotics.com>
2024-02-05 21:37:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "arremotefieldops-45999",
+  url: "https://arremotefieldops-45999.botics.co",
+  api: "https://arremotefieldops-45999.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
